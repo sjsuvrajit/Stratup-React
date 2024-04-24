97ecb138668fb85046922cda833b9ccf46094a30
Suvrajit Ghosh <sj.suvrajit.sg@gmail.com>
2024-04-24 22:49:46 +0530

    Modified the script and add JSX

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
 
-// creating Element in React, second param is the place of attribute
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React!"
-);
+//JSX - HTML-like or XML-like syntax
+const jsxHeading = (<h1 id="heading">Nameste Suvra from JSX 🚀</h1>)
 
-console.log(heading);
-//creating root to push the "h1" tag inside div
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//root.render(heading);
-
-/*
-    create this by using React
-    <div id="parent">
-        <div id="child">
-            <h1>I am a h1 tag</h1>
-            <h2>I am a h2 tag</h2>
-        </div>
-        <div id="child2">
-            <h1>I am a h1 tag</h1>
-            <h2>I am a h2 tag</h2>
-        </div>
-    </div>
-*/
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ]),
-]);
-
-root.render(parent);
+root.render(jsxHeading)
