b9a5410eb6f63d738ab3e4296e1d889fda991c91
Suvrajit Ghosh <sj.suvrajit.sg@gmail.com>
2024-04-27 13:19:12 +0530

    Add React component

--- App.js
+++ App.js
@@ -2,10 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client"; 
 
 //JSX - HTML-like or XML-like syntax
-const jsxHeading = (<h1 id="heading">Nameste Suvra from JSX 🚀</h1>)
+//React Element 
+const JsxHeading = () => (<h1 id="heading">Nameste Suvra from JSX 🚀</h1>)
 
-console.log(jsxHeading);
+console.log(JsxHeading);
+
+//React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+   {/* component composition: A component is used in other component     */}
+  < JsxHeading/>  
+  <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading)
+//this is how React Element render
+//root.render(JsxHeading);
+
+//this is how React Functional Compont render
+root.render(< HeadingComponent/>);
